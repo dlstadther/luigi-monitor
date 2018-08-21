962dc4816f7cf9ade851131b56944018d5154226
Dillon Stadther <dlstadther@gmail.com>
2018-08-20 20:41:19 -0400

    Add logging, remove unnecessary logic, add config class, remove parser, update string formatting

--- luigi_monitor/luigi_monitor.py
+++ luigi_monitor/luigi_monitor.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import inspect
 import json
+import logging
 import os
 import sys
 import platform
@@ -11,9 +12,17 @@ import luigi
 from luigi.retcodes import run_with_retcodes as run_luigi
 import requests
 
+logger = logging.getLogger('luigi-interface')
+
 const_success_message = "Task ran successfully!"
 const_failed_message = "Task failed!"
 const_missing_message = "Task could not be completed!"
+
+
+class luigi_monitor(luigi.Config):
+    slack_url = luigi.Parameter(default=None)
+    max_print = luigi.IntParameter(default=5)
+    username = luigi.Parameter(default=None)
 
 
 class Monitor:
@@ -72,7 +81,7 @@ def success(task):
     m.recorded_events['FAILURE'] = [failure for failure in m.recorded_events['FAILURE']
                                     if task not in failure['task']]
     m.recorded_events['DEPENDENCY_MISSING'] = [missing for missing in m.recorded_events['DEPENDENCY_MISSING']
-                                    if task not in missing]
+                                               if task not in missing]
     m.recorded_events['SUCCESS'].append(task)
 
 
@@ -97,13 +106,14 @@ def set_handlers(events):
         raise Exception("events must be a list")
 
     for event in events:
-        if not event in event_map:
+        if event not in event_map:
             raise Exception("{} is not a valid event.".format(event))
         handler = event_map[event]['handler']
         function = event_map[event]['function']
         luigi.Task.event_handler(handler)(function)
 
 
+# TODO: add configurability
 def format_message(max_print, host):
     job = os.path.basename(inspect.stack()[-1][1])
 
@@ -111,30 +121,34 @@ def format_message(max_print, host):
         host = platform.node()
     text = []
     emoji = ":x:"
+
     if m.has_failed_tasks() and 'FAILURE' in m.notify_events:
         text.append(add_context_to_message("failed", const_failed_message))
         text.append("\t\t\t*Failures:*")
         if len(m.recorded_events['FAILURE']) > int(max_print):
-            text.append("\t\t\tMore than %d failures. Please check logs." % max_print)
+            text.append("\t\t\tMore than {} failures. Please check logs.".format(max_print))
         else:
             for failure in m.recorded_events['FAILURE']:
                 text.append("\t\t\t\tTask: {}; Exception: {}".format(failure['task'], failure['exception']))
+
     if m.has_missing_tasks() and 'DEPENDENCY_MISSING' in m.notify_events:
         text.append(add_context_to_message("could not be completed", const_missing_message))
         text.append("\t\t\t*Tasks with missing dependencies:*")
         if len(m.recorded_events['DEPENDENCY_MISSING']) > int(max_print):
-            text.append("\t\t\t\tMore than %d tasks with missing dependencies. Please check logs." % max_print)
+            text.append("\t\t\t\tMore than {} tasks with missing dependencies. Please check logs.".format(max_print))
         else:
             for missing in m.recorded_events['DEPENDENCY_MISSING']:
                 text.append("\t\t\t\t" + missing)
+
     # if job successful add success message
     if m.is_success_only() and 'SUCCESS' in m.notify_events:
         emoji = ":heavy_check_mark:"
         text.append(add_context_to_message("ran successfully", const_success_message))
-        text.append("\t\t\t*Following %d tasks succeeded:*" % len(m.recorded_events['SUCCESS']))
+        text.append("\t\t\t*Following {} tasks succeeded:*".format(len(m.recorded_events['SUCCESS'])))
         for succeeded in m.recorded_events['SUCCESS']:
-            text.append("\t\t\t\t" + succeeded)
-    fulltext = [emoji + " Status report for {} at *{}*:".format(job, host)]
+            text.append("\t\t\t\t{}".format(succeeded))
+
+    fulltext = ["{} Status report for {} at *{}*:".format(emoji, job, host)]
     fulltext.extend(text)
     formatted_text = "\n".join(fulltext)
     if formatted_text == fulltext[0]:
@@ -145,36 +159,42 @@ def format_message(max_print, host):
 def add_context_to_message(result, appendix):
     if m.root_task is None:
         return appendix
+
+    message = ["\t\tTask *{}* ".format(m.root_task)]
+    if m.core_module is not None:
+        message.append("from module *{}* ".format(m.core_module))
+
+    message.append(result)
+
+    if m.root_task_parameters is not None:
+        message.append(" with parameters:")
+        for task_param in m.root_task_parameters:
+            if task_param.startswith("--"):
+                message.append("\n\t\t\t\t{}".format(task_param))
+            else:
+                message.append(" {}".format(task_param))
     else:
-        message = ["\t\tTask *" + m.root_task + "* "]
-        if m.core_module is not None:
-            message.append("from module *" + m.core_module + "* ")
-        message.append(result)
-        if m.root_task_parameters is not None:
-            message.append(" with parameters:")
-            for string in m.root_task_parameters:
-                if string.startswith("--"):
-                    message.append("\n\t\t\t\t" + string)
-                else:
-                    message.append(" " + string)
-        else:
-            message.append("!")
-        return ''.join(message)
+        message.append("!")
+    return ''.join(message)
 
 
-def send_message(slack_url, max_print, username=None, host=None):
-    text = format_message(max_print, host)
-    if not slack_url and text:
-        print("slack_url not provided. Message will not be sent")
-        print(text)
+def send_message(**kwargs):
+    slack_url = kwargs.get('slack_url')
+    max_print = kwargs.get('max_print')
+    username = kwargs.get('username')
+    host = kwargs.get('host')
+
+    msg = format_message(max_print, host)
+    if not slack_url:
+        logger.warn("slack_url not provided. Message will not be sent.\n{}".format(msg))
         return False
-    if text:
-        payload = {"text": text}
-        if username:
-            payload['username'] = username
-        r = requests.post(slack_url, data=json.dumps(payload))
-        if not r.status_code == 200:
-            raise Exception(r.text)
+
+    payload = {"text": msg}
+    if username:
+        payload['username'] = username
+    r = requests.post(slack_url, data=json.dumps(payload))
+    if r.status_code != 200:
+        raise Exception(r.text)
     return True
 
 
@@ -187,29 +207,20 @@ def monitor(events=['FAILURE', 'DEPENDENCY_MISSING', 'SUCCESS'], slack_url=None,
         m.notify_events = events
         set_handlers(events)
     yield m
-    send_message(slack_url, max_print, username, host)
+    kwargs = {'slack_url': slack_url, 'max_print': max_print, 'username': username, 'host': host}
+    send_message(**kwargs)
 
 
 def run():
     """Command line entry point for luigi-monitor"""
     events = ['FAILURE', 'DEPENDENCY_MISSING', 'SUCCESS']
-    slack_url, max_print, username = parse_config()
     m.notify_events = events
     set_handlers(events)
     parse_sys_args(sys.argv)
     try:
         run_luigi(sys.argv[1:])
     except SystemExit:
-        send_message(slack_url, max_print, username)
-
-
-def parse_config():
-    """Parse luigi-monitor config"""
-    config = luigi.configuration.get_config()
-    slack_url = config.get('luigi-monitor', 'slack_url', None)
-    max_print = config.get('luigi-monitor', 'max_print', 5)
-    username = config.get('luigi-monitor', 'username', None)
-    return slack_url, max_print, username
+        send_message(luigi_monitor.get_params())
 
 
 def parse_sys_args(args):
